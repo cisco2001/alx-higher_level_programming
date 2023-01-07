7760f932998cc828efdb6b053f0ad150668e7231
Mtwa Johakim Mgimwa <fransisco2001johakim@gmail.com>
2023-01-07 21:46:10 +0300

    make script executable

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    """ function that returns a tuple with the length of a string and its first character
+    """ returns a tuple with the length of a string and its first character
 
         :sentence: a string
     """
