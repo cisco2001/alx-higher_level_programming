0e0d55e6882a424270a6398fb502b27c699f48e8
Mtwa Johakim Mgimwa <fransisco2001johakim@gmail.com>
2023-01-07 21:44:00 +0300

    use tuple for multiple returns

---
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def multiple_returns(sentence):
+    """ function that returns a tuple with the length of a string and its first character
+
+        :sentence: a string
+    """
+    string_length = len(sentence)
+    if string_length == 0:
+        return (string_length, None)
+    return (string_length, sentence[0])
