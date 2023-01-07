77ff272403d72cd9b0616405fecb640568a4305f
Mtwa Johakim Mgimwa <fransisco2001johakim@gmail.com>
2023-01-07 16:47:48 +0300

    add doc string

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
-def print_list_integer(my_list=[]):
+def print_list_integer(my_list=[]: list) -> None:
+    """function that prints all integers of a list
+
+    :my_list: integer list
+    """
     for integer in my_list:
-        print("{}".format(integer))
+        print("{:d}".format(integer))
