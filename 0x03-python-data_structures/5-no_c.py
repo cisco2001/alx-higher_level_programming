a51fbd414930dadd919c73878c99c325480add58
Mtwa Johakim Mgimwa <fransisco2001johakim@gmail.com>
2023-01-07 18:38:25 +0300

    fix code

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -4,9 +4,8 @@ def no_c(my_string: str) -> str:
 
         :my_string: a string with c and C characters
     """
-    if (my_string):
-        new_string = list(my_string)
-        for character in new_string:
-            if character in "cC":
-                new_string.remove(character)
-        return "".join(new_string)
+    new_string = list(my_string)
+    for character in new_string:
+        if character in "cC":
+            new_string.remove(character)
+    return "".join(new_string)
