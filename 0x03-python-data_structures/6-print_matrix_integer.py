07b95a0f22b2e45a379797e262d0ff2942b671fe
Mtwa Johakim Mgimwa <fransisco2001johakim@gmail.com>
2023-01-07 21:01:43 +0300

    don't print space at the end of the row

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -6,5 +6,7 @@ def print_matrix_integer(matrix: list = [[]]) -> None:
     """
     for row in matrix:
         for element in row:
-            print("{:d}".format(element), end=" ")
+            print("{:d}".format(element), end="")
+            if row.index(element) < len(row) - 1:
+                print(" ", end="")
         print()
