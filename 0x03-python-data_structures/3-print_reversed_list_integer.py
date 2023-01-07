2ba8dcc6a2bafdde747727dd68b582615dad90c6
Mtwa Johakim Mgimwa <fransisco2001johakim@gmail.com>
2023-01-07 17:46:56 +0300

    check if list is None

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -4,6 +4,7 @@ def print_reversed_list_integer(my_list: list = []) -> None:
 
         :my_list: list of integers
     """
-    my_list.reverse()
-    for integer in my_list:
-        print("{:d}".format(integer))
+    if (my_list):
+        my_list.reverse()
+        for integer in my_list:
+            print("{:d}".format(integer))
