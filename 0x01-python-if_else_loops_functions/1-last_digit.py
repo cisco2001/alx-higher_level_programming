03dbbf61f246397c620cdeb88098987bd0a059d0
Mtwa Mgimwa <mtwa2001@gmail.com>
2024-01-16 14:06:17 +0300

    script runs

---
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+import random
+number = random.randint(-10000, 10000)
+last_digit = None
+if number < 0:
+    last_digit = (number % -10) * -1
+else:
+    last_digit = number % 10
+print(last_digit)
+string = "Last digit of"
+if last_digit > 5:
+    print(f"{string} {number:d} is {last_digit:d} and is greater than 5")
+elif last_digit == 0:
+    print(f"{string} {number:d} is {last_digit:d} and is 0")
+elif last_digit < 6 and not 0:
+    print(f"{string} {number:d} is {last_digit:d} and is less than 6 and not 0")
