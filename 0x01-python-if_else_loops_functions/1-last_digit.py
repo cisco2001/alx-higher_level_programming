d30727f0e85243783aa9aa3bbd3fc03e545539ab
Mtwa Mgimwa <mtwa2001@gmail.com>
2024-01-16 14:09:39 +0300

    conform to pycodestyle

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -9,8 +9,8 @@ else:
 print(last_digit)
 string = "Last digit of"
 if last_digit > 5:
-    print(f"{string} {number:d} is {last_digit:d} and is greater than 5")
+    print(f"{string} {number} is {last_digit} and is greater than 5")
 elif last_digit == 0:
-    print(f"{string} {number:d} is {last_digit:d} and is 0")
+    print(f"{string} {number} is {last_digit} and is 0")
 elif last_digit < 6 and not 0:
-    print(f"{string} {number:d} is {last_digit:d} and is less than 6 and not 0")
+    print(f"{string} {number} is {last_digit} and is less than 6 and not 0")
