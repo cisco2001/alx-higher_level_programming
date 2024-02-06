bc034bf22291a2006235c42c36518be427b62d2b
Mtwa Mgimwa <mtwa2001@gmail.com>
2024-02-06 03:00:00 +0300

    use string formatting

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -7,5 +7,5 @@ def safe_print_integer_err(value):
         print("{:d}".format(value))
         return True
     except ValueError as e:
-        print("Exception:", e.__str__(), file=sys.stderr)
+        print("Exception: {}".format(e.__str__()), file=sys.stderr)
         return False
