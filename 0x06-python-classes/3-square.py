28062bc11b2407fe3296d523f500368aefc36ee4
Mtwa Mgimwa <mtwa2001@gmail.com>
2024-02-07 23:55:35 +0300

    script runs

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+""" This module demonstrates use of private instance attributes
+    together with instantiation
+"""
+
+
+class Square:
+    """ square with specified size, size must be integer and greater than zero
+    """
+    def __init__(self, size=0):
+        if isinstance(size, int):
+            if size >= 0:
+                self.__size = size
+            else:
+                raise ValueError("size must be >= 0")
+        else:
+            raise TypeError("size must be an integer")
+
+    def area(self):
+        """ computes area of a square from the size above
+        """
+        return self.__size ** 2
