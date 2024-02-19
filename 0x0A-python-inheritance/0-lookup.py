abb3b4e2df89b0e6ab65fb271f63be1aa91cc350
Mtwa Mgimwa <mtwa2001@gmail.com>
2024-02-19 22:22:32 +0300

    check

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
-""" This is a module consisting of a function that can be used to check attributes and methods of an object
+""" This module that can be used to check attributes and methods of an object
 """
 
 
 def lookup(obj):
-	return obj.__dir__()
+    """ This function returns a list of attributes and methods of 'obj'
+    """
+    return obj.__dir__()
